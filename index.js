47c3758e2c063f13fdd1852d00b9ddb3a92c19a7
wassim triki <wsmtriki@gmail.com>
2022-08-18 09:35:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  console.log(interaction);
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
