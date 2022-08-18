20f1ffafbb399606ce2fac47ecef52226fa4ec98
wassim triki <wsmtriki@gmail.com>
2022-08-18 14:25:01 +0100

    add discordjs rest and initial commands

--- index.js
+++ index.js
@@ -2,14 +2,61 @@
 require('dotenv').config();
 const config = require('./config');
 const player = require('./player');
+const { REST, Routes } = require('discord.js');
 const client = require('./client');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const {
+  joinVoiceChannel,
+  createAudioResource,
+  createAudioPlayer,
+  StreamType,
+} = require('@discordjs/voice');
 
 const SERVER_ID = '1009727529122267176';
 const WELCOME_CHANNEL_ID = '1009745835908669501';
 const GENERAL_CHANNEL_ID = '1009763648870301796';
 
 client.player = player;
+
+const commands = [
+  {
+    name: 'play',
+    description: 'Play a song.',
+    options: [
+      {
+        name: 'name',
+        description: 'The name of the song',
+        type: 3, //String
+      },
+    ],
+  },
+];
+const rest = new REST({ version: '10' }).setToken(config.token);
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+      body: commands,
+    });
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'play') {
+    const { _hoistedOptions: options } = interaction.options;
+    const songName = options[0]?.value || null;
+    let reply = 'Playing a random song 🎶... ';
+    if (songName) {
+      reply = `Playing "${songName}" 🎶...`;
+    }
+    await interaction.reply(reply);
+  }
+});
 
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -32,22 +79,33 @@ client.on('guildMemberRemove', (member) => {
   generalChannel.send(`<@${user.id}> **5raj**/**tza3ak** 🤡 mel *Denya*.`);
 });
 
-client.on('messageCreate', async (message) => {
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift();
-  const guildQueue = client.player.getQueue(message.guild.id);
+// client.on('messageCreate', async (message) => {
+//   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+//   const command = args.shift();
+//   const guildQueue = client.player.getQueue(message.guild.id);
 
-  if (command === 'play') {
-    let queue = client.player.createQueue(message.guild.id);
-    const channel = message.member.voice.channel;
-    if (!channel)
-      message.channel.send('a5tar voice channel bch tal3b mouzika.');
-    const connection = joinVoiceChannel({
-      channelId: channel.id,
-      guildId: message.guild.id,
-      adapterCreator: message.guild.voiceAdapterCreator,
-    });
-  }
-});
+//   if (command === 'play') {
+//     let queue = client.player.createQueue(message.guild.id);
+//     const channel = message.member.voice.channel;
+//     if (!channel)
+//       message.channel.send('a5tar voice channel bch tal3b mouzika.');
+//     const connection = joinVoiceChannel({
+//       channelId: channel.id,
+//       guildId: message.guild.id,
+//       adapterCreator: message.guild.voiceAdapterCreator,
+//       selfDeaf: false,
+//     });
+//     const stream = ytdl(
+//       'https://www.youtube.com/watch?v=KmVaSZS_SW4&ab_channel=PolyvinylRecords',
+//       { filter: 'audioonly' }
+//     );
+//     const resource = createAudioResource(stream, {
+//       inputType: StreamType.Arbitrary,
+//     });
+//     const audioPlayer = createAudioPlayer();
+//     audioPlayer.play(resource);
+//     connection.subscribe(audioPlayer);
+//   }
+// });
 
 client.login(config.token);
