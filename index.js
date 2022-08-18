1b7975f1bac2881f9c8535c22328bfb7ae3ed9ba
wassim triki <wsmtriki@gmail.com>
2022-08-18 11:47:32 +0100

    add welcome and remove member bot message

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+'use strict';
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+  partials: [Partials.Channel],
+});
 
-client.on('ready', () => {
+const SERVER_ID = '1009727529122267176';
+const WELCOME_CHANNEL_ID = '1009745835908669501';
+const GENERAL_CHANNEL_ID = '1009763648870301796';
+
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-  console.log(interaction);
-  if (!interaction.isChatInputCommand()) return;
+client.on('guildMemberAdd', (member) => {
+  const welcomeChannel = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
+  if (!welcomeChannel) return;
+  const role = member.guild.roles.cache.find((role) => role.name === 'Peasant');
+  if (role) member.roles.add(role);
+  const { user } = member;
+  welcomeChannel.send(`3aslema <@${user.id}> 🙌 ! Ahla bik fel **Denya**.`);
+});
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+client.on('guildMemberRemove', (member) => {
+  const generalChannel = member.guild.channels.cache.get(GENERAL_CHANNEL_ID);
+
+  if (!generalChannel) return;
+  const { user } = member;
+  generalChannel.send(`<@${user.id}> **5raj**/**tza3ak** 🤡 mel *Denya*.`);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
